7b88b0cd56ae2dda281d63b7440646ac8a809e51
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-11-22 06:52:42 -0500

    social buttons

--- sensitive/homepage-top.php
+++ sensitive/homepage-top.php
@@ -9,7 +9,20 @@
 <h2><?php echo sensitive_get_theme_opts('home_featured_title',"Sensitive"); ?></h2>
 <p>
 <?php echo nl2br(wptexturize(sensitive_get_theme_opts('home_featured_desc',"Sensitive, Fully Responsive WordPress Theme using Bootstrap"))); ?> 
-</p><br>
+</p>
+<p class="social">
+  <span>
+    <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+  </span>
+
+  <span>
+    <g:plusone></g:plusone>
+  </span>
+
+  <span>
+    <iframe src="https://ghbtns.com/github-btn.html?user=jQueryGeo&repo=geo&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe>
+  </span>
+</p>
  <!--center>
          <a href="<?php echo sensitive_get_theme_opts('home_featured_btnurl','#'); ?>" class="btn btn-large <?php echo sensitive_get_theme_opts('button_style','btn-info'); ?>"><i class="icon-fire icon-white"></i> <?php echo sensitive_get_theme_opts('home_featured_btntxt','Get It Now!'); ?></a>
 </center-->
